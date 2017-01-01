587aa7ca77398a9dfff752daedc9537e4ba1b2d7
muoivpham <muoi@niftit.com>
2016-12-31 19:52:43 -0600

    first commit

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,22 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by muoivpham on 12/31/16.
+//  Copyright © 2016 muoivpham. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    func setOperand(operand: Double){}
+    
+    func performOperation(symbol: String) {}
+    
+    var result: Double {
+        get {
+            return 0.0
+        }
+    }
+}
